34542c3aabb91ac6230ba96d5fb072d73bfce63a
Bhupendra Singh <bhsingh@gmail.com>
2014-10-10 16:32:12 +0530

    Refactored test worker.

--- worker.py
+++ worker.py
@@ -1,22 +1,32 @@
 import json
 import os
 
-def locktest():
-    fd = os.open('/tmp/conductor.locktest', os.O_CREAT|os.O_RDWR)
+def locktest(input):
+    index = ('%010d' % (input['worker']))[9]
+
+    fd = os.open('/tmp/locktest.' + index, os.O_CREAT|os.O_RDWR)
 
     raw  = os.read(fd, 1000000)
 
     if (raw is None) or ('' == raw):
-        obj = dict(count=0)
+        obj = dict(total=0, workers=list(), guid=input['guid'])
+        os.ftruncate(fd, 0)
     else:
         obj = json.loads(raw)
+        if ('guid' not in obj) or (input['guid'] != obj['guid']):
+            obj = dict(total=0, workers=list(), guid=input['guid'])
+            os.ftruncate(fd, 0)
 
-    obj['count'] += 1
+    obj['total'] += 1
+    obj['workers'].append(input['worker'])
 
     os.lseek(fd, 0, os.SEEK_SET)
-    os.write(fd, json.dumps(obj))
+    os.write(fd, json.dumps(obj, indent=4, sort_keys=True))
 
 def worker(input, state, event):
+    input = json.loads(input)
+    index = ('%010d' % (input['worker']))[9]
+
     if 'init' == event['code']:
         return dict(status='received init',
                     state=json.dumps(dict(seq=0)),
@@ -28,10 +38,10 @@ def worker(input, state, event):
     if 1 == state['seq']:
         return dict(status='alarm',
                     state=json.dumps(state),
-                    lock='locktest')
+                    lock='locktest-' + index)
 
     if (2 == state['seq']) and ('locked' == event['code']):
-        locktest()
+        locktest(input)
         return dict(status='alarm',
                     state=json.dumps(state),
                     alarm=3)
@@ -39,7 +49,7 @@ def worker(input, state, event):
     if 3 == state['seq']:
         return dict(status='alarm',
                     state=json.dumps(state),
-                    unlock='locktest',
+                    unlock='locktest-' + index,
                     alarm=0)
     
     if 'alarm' == event['code']:
