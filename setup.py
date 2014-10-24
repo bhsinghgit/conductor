9e107cc88d09d229e25e0abf156697aaba754a82
Bhupendra Singh <bhsingh@gmail.com>
2014-10-24 17:22:04 +0530

    Tested with an application installed in a separate virtualenv.

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     author='Bhupendra Singh',
     url='https://github.com/bhsinghgit/shepherd',
     author_email='bhsingh@gmail.com',
-    version='0.1',
+    version='0.3',
     packages=['shepherd'],
     scripts=['bin/shepherd', 'bin/cli', 'bin/worker'],
-    py_modules=['worker', 'sheepdog', 'sheep'],
+    py_modules=['logger', 'worker', 'sheepdog', 'sheep'],
     name='shepherd'
 )
