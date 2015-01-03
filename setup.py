3489bf7f851b88cefb8e3cb8cb72e3ad8f1b5e47
Bhupendra Singh <bhsingh@gmail.com>
2015-01-03 19:17:39 +0530

    removing shepherd_schema from setup file

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setuptools.setup(
     author='Bhupendra Singh',
     url='https://github.com/bhsinghgit/shepherd',
     author_email='bhsingh@gmail.com',
-    version='0.34',
+    version='0.35',
     install_requires=['flask', 'pymysql', 'gunicorn'],
-    scripts=['bin/shepherd', 'bin/shepherd_api', 'bin/shepherd_schema'],
+    scripts=['bin/shepherd', 'bin/shepherd_api'],
     name='shepherd'
 )
